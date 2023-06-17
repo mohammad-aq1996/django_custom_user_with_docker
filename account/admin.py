e148a6ff0935474a72c723587d0fd28e896561c4
mohammad-aq1996 <mohammad.aq1996@gmail.com>
2023-06-17 21:42:11 +0330

    [add] add custom user

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,65 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django import forms
+from .models import MyUser
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-# Register your models here.
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password Confirm", widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = ('email',
+                  'password1', 
+                  'password2'
+                  )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Your password is not match")
+        return password2
+    
+    def save(self, commit=True):
+        user = super().save(commit)
+        user.set_password(self.cleaned_data.get("password2"))
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text="You could change password via <a href=\"../password/\"> this link</a>.")
+
+    class Meta:
+        model = MyUser
+        fields = ('email', )
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserUpdateForm
+    add_form = UserCreationForm
+
+    list_display = ["email", "fullname", "is_staff", "is_superuser"]
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(MyUser, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValidationError('Please enter email')
+        user = MyUser(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_staff(self, email, password, fullname=None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, fullname=None):
+        user = self.create_staff(email, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(unique=True)
+    fullname = models.CharField(max_length=256)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    object = MyUserManager()
+
+    def __str__(self):
+        return self.email
