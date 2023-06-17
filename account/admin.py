763f21a224ef8067d7b4b639feefe2f8a5fe3faa
mohammad-aq1996 <mohammad.aq1996@gmail.com>
2023-06-17 21:46:26 +0330

    [add] account/admin: add 'is_superuser' to UserAdmin fieldsets

--- account/admin.py
+++ account/admin.py
@@ -47,7 +47,7 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
     )
     add_fieldsets = (
         (None, {
